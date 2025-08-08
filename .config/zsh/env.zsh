b95673434984a8a1ae41db4a7b460a87f88a045c
Santiago Ramis <santiago1.ramis@volkswagen-groupservices.com>
2025-08-08 09:00:29 +0200

    update files

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -1,21 +1,17 @@
 eval "$(starship init zsh)"
 
-HOMEBREW_CELLAR=/opt/homebrew/Cellar
-HOMEBREW_PREFIX=/opt/homebrew
-HOMEBREW_REPOSITORY=/opt/homebrew
+# Homebrew
+export HOMEBREW_CELLAR=/opt/homebrew/Cellar
+export HOMEBREW_PREFIX=/opt/homebrew
+export HOMEBREW_REPOSITORY=/opt/homebrew
 
-LDFLAGS="-L/opt/homebrew/opt/mysql-client/lib"
-CPPFLAGS="-I/opt/homebrew/opt/mysql-client/include"
+# MySQL client
+export LDFLAGS="-L/opt/homebrew/opt/mysql-client/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/mysql-client/include"
 
-NPM_EMAIL=santiago1.ramis@volkswagen-groupservices.com
-NPM_TOKEN=YW92eThoczpBS0NwQnU0dnJONnVTZzhFZ2ZVTlNGZlRmVXA4TlFDWHNCNmJ4NHZDUHU5SzZIRFoyMXhYNFVnZ2VpOWQ1dkQycXlib3U1d3c5
+# NPM
+export NPM_EMAIL="santiago1.ramis@volkswagen-groupservices.com"
+export NPM_TOKEN="YW92eThoczpBS0NwQnU0dnJONnVTZzhFZ2ZVTlNGZlRmVXA4TlFDWHNCNmJ4NHZDUHU5SzZIRFoyMXhYNFVnZ2VpOWQ1dkQycXlib3U1d3c5"
 
-EDITOR=vim 
-
-plugins=(
-    z
-    zsh-autosuggestions
-    zsh-syntax-highlighting
-    docker
-    docker-compose
-)
+# Editor
+export EDITOR=nvim
